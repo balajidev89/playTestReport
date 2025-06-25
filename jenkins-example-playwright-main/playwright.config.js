774b093aa32a2e539d78ae270a73af8f019b3579
test-local <mailtobalaji89@gmail.com>
2025-06-25 10:58:34 +0530

    commit

--- jenkins-example-playwright-main/playwright.config.js
+++ jenkins-example-playwright-main/playwright.config.js
@@ -1,110 +1,81 @@
-import { PlaywrightTestConfig, devices } from '@playwright/test';
+// @ts-check
+const { devices } = require('@playwright/test');
+import {AllureReporter} from  "allure-playwright"
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
-const config: PlaywrightTestConfig = {
-
-  testDir: './tests',
-
-  /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
-
-  expect: {
-
-    /**
-     * Maximum time expect() should wait for the condition to be met.
-     * For example in `await expect(locator).toHaveText();`
-     */
-    timeout: 5000
-  },
-
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 0,
-
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
-
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-
-      /* Project-specific settings. */
-      use: {
-        ...devices['Desktop Chrome'],
-      },
+const config = {
+    // workers: process.env.CI ? 4 : 1,
+    testDir: './tests',
+    timeout: 30 * 1000,
+    expect: {
+        timeout: 5000,
     },
-
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-      },
-    },
-
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-      },
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: {
-    //     ...devices['iPhone 12'],
-    //   },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: {
-    //     channel: 'msedge',
-    //   },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: {
-    //     channel: 'chrome',
-    //   },
-    // },
-  ],
-
-  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+    retries: 1,
+    reporter: "allure-playwright",
+    resultsDir: "allure-results",
+    projects: [
+        {
+            name: 'Chrome',
+            use: {
+                browserName: 'chromium',
+                headless: true,
+                screenshot: 'on',
+                video: 'retain-on-failure',
+                trace: 'retain-on-failure',
+            },
+        },
+        {
+            name: 'Firefox',
+            use: {
+                browserName: 'firefox',
+                headless: false,
+                screenshot: 'off',
+                trace: 'off',
+                viewport: {
+                    width: 1920,
+                    height: 1080,
+                },
+            },
+        },
+        {
+            name: 'Safari',
+            use: {
+                browserName: 'webkit',
+                headless: false,
+                screenshot: 'on',
+                trace: 'retain-on-failure',
+            },
+        },
+        {
+            name: 'iPhone',
+            use: {
+                browserName: 'webkit',
+                headless: false,
+                screenshot: 'on',
+                trace: 'retain-on-failure',
+                ...devices['iPhone 11'],
+            },
+        },
+        {
+            name: 'ignoreHttpsErrors',
+            use: {
+                browserName: 'webkit',
+                headless: false,
+                screenshot: 'on',
+                trace: 'retain-on-failure',
+                ignoreHttpsErros: true,
+            },
+        },
+        {
+            name: 'differentPermission',
+            use: {
+                browserName: 'webkit',
+                headless: false,
+                screenshot: 'on',
+                trace: 'retain-on-failure',
+                permissions: ['geolocation', 'notifications'],
+            },
+        },
+    ],
 };
-export default config;
+
+module.exports = config;
