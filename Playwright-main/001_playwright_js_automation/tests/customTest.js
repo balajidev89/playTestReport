2c2793033df596a4e693891a3f689bfc680ba0d9
R BALAJI <83776077+balajidev89@users.noreply.github.com>
2025-07-01 17:54:51 +0530

    Delete Playwright-main directory

--- Playwright-main/001_playwright_js_automation/tests/customTest.js
+++
@@ -1,9 +0,0 @@
-const playwright = require('@playwright/test');
-
-exports.customTest = playwright.test.extend({
-    user: {
-        email: 'fakeaccount@getnada.com',
-        password: 'superfake1B',
-        productName: 'adidas original',
-    },
-});
